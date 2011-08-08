cd710c8c91b0593a829cacabee04378ebce78833
Marko Stijak <mstijak@gmail.com>
2011-08-08 09:46:37 +0200

    fixing nugets in git repo

--- NuGet/build.bat
+++ NuGet/build.bat
@@ -1,5 +1,8 @@
-copy ..\Libraries\Codaxy.Common\bin\Release\*.* Codaxy.Common\lib
-copy ..\Libraries\Codaxy.Common.Localization\bin\Release\Codaxy.Common.Localization.* Codaxy.Common.Localization\lib
+mkdir Codaxy.Common\lib\
+mkdir Codaxy.Common.Localization\lib\
+
+copy ..\Libraries\Codaxy.Common\bin\Release\*.* Codaxy.Common\lib\
+copy ..\Libraries\Codaxy.Common.Localization\bin\Release\Codaxy.Common.Localization.* Codaxy.Common.Localization\lib\
 
 tools\nuget pack Codaxy.Common\Codaxy.Common.nuspec
 tools\nuget pack Codaxy.Common.Localization\Codaxy.Common.Localization.nuspec
